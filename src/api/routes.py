9f16ad4ce588912d7eaa45f3ba430a44dcc8bec5
tmayolgomila <tmayolgomila@gmail.com>
2022-07-22 19:38:50 +0000

    empezando el proyecto

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,12 +4,14 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from flask_jwt_extended import create_access_token
+
 
 api = Blueprint('api', __name__)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@api.route('/token', methods=['POST'])
+def create_token():
 
     response_body = {
         "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
---
+++ src/front/js/pages/private.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect, useContext } from "react";
+import PropTypes from "prop-types";
+import { Link, useParams } from "react-router-dom";
+import { Context } from "../store/appContext";
+import rigoImageUrl from "../../img/rigo-baby.jpg";
+
+export const Private = props => {
+	const { store, actions } = useContext(Context);
+	const params = useParams();
+
+	return (
+		<h1>PRIVATE</h1>
+	);
+};
+
+
