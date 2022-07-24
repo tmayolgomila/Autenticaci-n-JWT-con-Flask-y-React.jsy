f5199dd85d5eebf7ff567b1a95eab8c2fbb4b29a
tmayolgomila <tmayolgomila@gmail.com>
2022-07-24 16:09:38 +0000

    subiendo cambios

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,7 +4,8 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
-from flask_jwt_extended import create_access_token
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
+
 
 
 api = Blueprint('api', __name__)
@@ -12,9 +13,22 @@ api = Blueprint('api', __name__)
 
 @api.route('/token', methods=['POST'])
 def create_token():
+    username = request.json.get("username", None)
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    #Consultamos base de datos por email y contraseña
+    user = User.filter.query(username=username, email=email, password=password).first()
+    if user is None:
+        return jsonify({"msg":"error in the username, email or password"}), 401
+    #Creamos un nuevo token con el id del usuario
+    access_token = create_access_token( identity=user.id)
+    return jsonify({"token":access_token, "user_id":user.id}), 200
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
-
-    return jsonify(response_body), 200
+@api.route("/private", methods=['GET'])
+@jwt_required()
+def private():
+    # Accede a la identidad del usuario actual con get_jwt_identity
+    current_user_id = get_jwt_identity()
+    user = User.filter.get(current_user_id)
+    
+    return jsonify({"id": user.id, "email": user.email }), 200
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -16,10 +16,29 @@ const getState = ({ getStore, getActions, setStore }) => {
 			]
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
+			 setUsers : async (username,email, password) => {
+				const resp = await fetch(`https://3001-tmayolgomil-jwtconflask-1mt3mvq5b43.ws-eu54.gitpod.io/token`, { 
+					 method: "POST",
+					 headers: { "Content-Type": "application/json" },
+					 body: JSON.stringify({ "username": username,"email":email, "password": password }) 
+				})
+		   
+				if(!resp.ok) throw Error("There was a problem in the login request")
+		   
+				if(resp.status === 401){
+					 throw("Invalid credentials")
+				}
+				else if(resp.status === 400){
+					 throw ("Invalid email or password format")
+				}
+				const data = await resp.json()
+				// save your token in the localStorage
+			   //also you should set your user into the store using the setStore function
+				localStorage.setItem("jwt-token", data.token);
+		   
+				return data
+		   },
+		
 
 			getMessage: async () => {
 				try{
@@ -32,20 +51,6 @@ const getState = ({ getStore, getActions, setStore }) => {
 				}catch(error){
 					console.log("Error loading message from backend", error)
 				}
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
 			}
 		}
 	};
